d41f4de4492c3f35947e0f5f47542d975dbe2079
Aniket3689 <148610749+aniketbcastudent@users.noreply.github.com>
2024-05-02 08:51:49 +0530

    add user model

---
+++ src/models/user.models.js
@@ -0,0 +1,46 @@
+import mongoose, { Schema } from "mongoose";
+const userchema=new Schema({
+      username:{
+        type:String,
+        require:true,
+        unique:true,
+        lowecase:true,
+        trim:true,
+        index:true
+      },
+      email:{
+        type:String,
+        require:true,
+        unique:true,
+        lowecase:true,
+        trim:true,
+      },
+      fullname:{
+        type:String,
+        require:true,
+        trim:true,
+        index:true,
+      },
+      avatar:{
+        type:String,
+        require:true,
+      },
+      coverImage:{
+        type:String,  
+      },
+      watchHistory:[
+        {
+            type:Schema.Types.ObjectId,
+            ref:"video"
+        }
+      ],
+      password:{
+            type:String,
+            require:[true,'password is requried']
+      },
+      refreshToken:{
+        type:string
+      },
+
+},{timestamps:true})
+export const User=mongoose.model("user",userchema)
